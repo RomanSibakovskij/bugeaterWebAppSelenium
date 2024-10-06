26fdba826739cc7cc0993fbc42f605c9b891c5da
RomanSibakovskij <j4023958@gmail.com>
2024-10-06 12:10:26 +0300

    Add empty ListOfChallengesPage sheet

---
+++ src/main/java/app/web/bugeater/ListOfChallengesPage.java
@@ -0,0 +1,11 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+
+public class ListOfChallengesPage extends BasePage{
+
+
+    public ListOfChallengesPage(WebDriver driver) {
+        super(driver);
+    }
+}
