5575690b8209080ea7f2fa2549610bf3df1c2889
RomanSibakovskij <j4023958@gmail.com>
2024-10-06 12:12:03 +0300

    Add empty TestMethods sheet

---
+++ src/test/java/app/web/bugeater/TestMethods.java
@@ -0,0 +1,11 @@
+package app.web.bugeater;
+
+import org.slf4j.*;
+
+public class TestMethods extends BaseTest{
+
+    protected static final Logger logger = LoggerFactory.getLogger(TestMethods.class);
+
+    
+
+}
