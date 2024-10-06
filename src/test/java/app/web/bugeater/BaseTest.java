d18309d1dbf8c85f1d1034464439908f6aba86e0
RomanSibakovskij <j4023958@gmail.com>
2024-10-06 12:06:43 +0300

    Add basic bugeaterWebAppSelenium project setup

---
+++ src/test/java/app/web/bugeater/BaseTest.java
@@ -0,0 +1,32 @@
+package app.web.bugeater;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.*;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-search-engine-choice-screen"); //for browser run
+        //options.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu", "--window-size=1920,1080"); //this is necessary for CI run, disable once run in IDE and re-enable commented out options
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize(); //-> enable this with browser run
+        driver.get("https://bugeater.web.app/list");
+    }
+
+    @AfterEach
+    public void close() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
