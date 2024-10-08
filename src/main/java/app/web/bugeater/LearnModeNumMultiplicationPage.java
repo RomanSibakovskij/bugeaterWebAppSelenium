01a35872869bf0692facf18a78f2b4005fdb4108
RomanSibakovskij <j4023958@gmail.com>
2024-10-08 09:46:17 +0300

    Add empty LearnModeNumMultiplicationPage sheet

---
+++ src/main/java/app/web/bugeater/LearnModeNumMultiplicationPage.java
@@ -0,0 +1,9 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+
+public class LearnModeNumMultiplicationPage extends BasePage{
+    public LearnModeNumMultiplicationPage(WebDriver driver) {
+        super(driver);
+    }
+}
