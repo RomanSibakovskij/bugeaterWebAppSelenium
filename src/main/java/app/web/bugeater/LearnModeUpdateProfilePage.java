cb3493877de1d36aa53b5f9293767e64e52a7106
RomanSibakovskij <j4023958@gmail.com>
2024-10-08 08:37:38 +0300

    Add empty LearnModeUpdateProfilePage sheet

---
+++ src/main/java/app/web/bugeater/LearnModeUpdateProfilePage.java
@@ -0,0 +1,9 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+
+public class LearnModeUpdateProfilePage extends BasePage{
+    public LearnModeUpdateProfilePage(WebDriver driver) {
+        super(driver);
+    }
+}
