19b1cce35569dc8ac18dc0f7d296be19b019500c
RomanSibakovskij <j4023958@gmail.com>
2024-10-08 06:25:56 +0300

    Add empty LearnModePasswordChallengePage sheet

---
+++ src/main/java/app/web/bugeater/LearnModePasswordChallengePage.java
@@ -0,0 +1,9 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+
+public class LearnModePasswordChallengePage extends BasePage{
+    public LearnModePasswordChallengePage(WebDriver driver) {
+        super(driver);
+    }
+}
