882da8958733b434fa8d96411bc349af85adef62
RomanSibakovskij <j4023958@gmail.com>
2024-10-10 07:27:57 +0300

    Add empty ScriptedTestingNumMultiplicationValidationChallengePage sheet

---
+++ src/main/java/app/web/bugeater/ScriptedTestingNumMultiplicationValidationChallengePage.java
@@ -0,0 +1,11 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+
+public class ScriptedTestingNumMultiplicationValidationChallengePage extends BasePage{
+
+    public ScriptedTestingNumMultiplicationValidationChallengePage(WebDriver driver) {
+        super(driver);
+    }
+    
+}
