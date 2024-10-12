16fad00c0147123cd5900b519c01cad56e84b38c
RomanSibakovskij <j4023958@gmail.com>
2024-10-12 08:18:52 +0300

    Add empty TCGNumSummationTCValidationChallengePage sheet

---
+++ src/main/java/app/web/bugeater/TCGNumSummationTCValidationChallengePage.java
@@ -0,0 +1,15 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class TCGNumSummationTCValidationChallengePage extends BasePage{
+
+    public TCGNumSummationTCValidationChallengePage(WebDriver driver) {
+        super(driver);
+    }
+
+}
