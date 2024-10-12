15ad1ac00181ded9f406879bbba17c668834e0c3
RomanSibakovskij <j4023958@gmail.com>
2024-10-12 06:35:42 +0300

    Add empty ScriptedTestingCurrencyConverterValidationChallengePage sheet

---
+++ src/main/java/app/web/bugeater/ScriptedTestingCurrencyConverterValidationChallengePage.java
@@ -0,0 +1,15 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class ScriptedTestingCurrencyConverterValidationChallengePage extends BasePage{
+
+    public ScriptedTestingCurrencyConverterValidationChallengePage(WebDriver driver) {
+        super(driver);
+    }
+    
+}
