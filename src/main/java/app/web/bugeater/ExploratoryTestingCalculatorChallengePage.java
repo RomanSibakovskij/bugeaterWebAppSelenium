753b435696a630fa3b9226c381c4f85e8345aa05
RomanSibakovskij <j4023958@gmail.com>
2024-10-14 07:50:14 +0300

    Add empty ExploratoryTestingCalculatorChallengePage sheet

---
+++ src/main/java/app/web/bugeater/ExploratoryTestingCalculatorChallengePage.java
@@ -0,0 +1,15 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class ExploratoryTestingCalculatorChallengePage extends BasePage {
+
+    public ExploratoryTestingCalculatorChallengePage(WebDriver driver) {
+        super(driver);
+    }
+
+}
