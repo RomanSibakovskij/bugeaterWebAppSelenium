200acf169e8fae8217fd6f8576af49b356523470
RomanSibakovskij <j4023958@gmail.com>
2024-10-15 13:39:31 +0300

    Add basic ExploratoryTestingBookADeskChallengePage sheet

---
+++ src/main/java/app/web/bugeater/ExploratoryTestingBookADeskChallengePage.java
@@ -0,0 +1,15 @@
+package app.web.bugeater;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class ExploratoryTestingBookADeskChallengePage extends BasePage{
+
+    public ExploratoryTestingBookADeskChallengePage(WebDriver driver) {
+        super(driver);
+    }
+
+}
